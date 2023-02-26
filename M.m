a48dc3e1b860d082072dca3c6c4baa20836baa2a
Alberto Bocchinfuso <axb1094@case.edu>
2023-02-26 10:48:48 +0100

    Scatter plot of the probablity to retrieve the matrix

--- M.m
+++ M.m
@@ -12,13 +12,12 @@ result = 0;
 for i=1:m
     a_i = A1(i,:);    
     pp = probability(a_i, a1_mean, A, a_mean);
-    
     if(pp >= 1)
         error()
     end
     
-    result = result + distance_2(a_i, a1_mean, M_max_1)*( 1 - probability(a_i, a1_mean, A, a_mean) );
+    result = result + distance_2(a_i, a1_mean, M_max_1)*( 1 - pp );
 end
-result/m
+result/m;
 
 end
--- Test.m
+++ Test.m
@@ -4,7 +4,24 @@ close all
 load brca_mRNA_1977patients.mat
 load brca_mRNA_1977patients_iteration_12_.mat
 
+M(A, A)
+
+[ m, n ] = size(A)
 priv_Bayes = M(A, A_1)
+
+a_mean = mean(A);
+a1_mean = mean(A_1);
+
+probs = zeros(1,m);
+for i=1:m
+    a_i = A_1(i,:);    
+    probs(i) = probability(a_i, a1_mean, A, a_mean);     
+end
+
+figure
+plot(probs,'.')
+title('Probaility of retrieving A, starting from Private Bayes privatized data')
+set(gca, 'FontSize', 16)
 
 load brca_mRNA_1977patients_iteration_12_.mat
 
@@ -13,3 +30,4 @@ BayNet = M(A, A_1)
 load brca_mRNA_patients_embedding_dim128_gen_dim256_dis_dim256_l2scale1e-06_batch_size500_epochs500.mat
 
 CTGAN = M(A, A_1)
+
